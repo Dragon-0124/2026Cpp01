778bf13b8fd8455072c543c3c546af0811a343ab
Dragon-0124 <skseog0841@gmail.com>
2026-04-30 11:29:21 +0900

    Week09: String V0.3.1 - Null Terminator

--- Week09/app01.cpp
+++ Week09/app01.cpp
@@ -8,15 +8,17 @@ int main() {
 	//char s1[] = { 'A', 'C', 'E', NULL };	
 	//char s1[] = { 'A', 'C', 'E', 0 };
 	char s1[] = "ACE";
-	char* ps1 = s1;
-	cout << s1 << '\n';			//ACE
-	cout << *ps1 << '\n';		//A
-	cout << ps1 << '\n';		//ACE
-	cout << *s1 << '\n';		//A
-	cout << s1[1] << '\n';		//C
-	cout << *(ps1+1) << '\n';	//C
+	
+	char* ps1 = &s1[0];			// ≒ char* ps1 = s1;
+	cout << s1 << '\n';			// ACE
+	cout << *ps1 << '\n';		// A
+	cout << ps1 << '\n';		// ACE
+	cout << *s1 << '\n';		// A
+	cout << s1[1] << '\n';		// C
+	cout << *(ps1+1) << '\n';	// C
 
-	*(ps1 + 2) = 'Z';
-	cout << s1 << '\n';			//ACE
+	
+	ps1[2] = 'Z';				// ≒ *(ps1 + 2) = 'Z';
+	cout << s1 << '\n';			// ACZ
 	return 0;
 }
