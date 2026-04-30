3b9d252d83e5618a5e31278cb4efe451dd593b70
Dragon-0124 <skseog0841@gmail.com>
2026-04-30 12:10:57 +0900

    Week09: String V0.5 - strlen

--- Week09/app01.cpp
+++ Week09/app01.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include <cstring>
+#include <cstring>  // string.h
 using namespace std;
 
+int myStrlen(const char* s) {
+	int count = 0;
+	while (s[count]) {  // (s[i] != 0), (s[i] != NULL), (s[count] != '\0')
+		count++;
+	}
+	return count;
+}
+
 int main() {
-	char s1[] = "ACE";			// array compact initializer
-	const char* ps1 = "ACE";	// string literal
+	char s1[] = "ACE";  // array compact initializer
+	const char* ps1 = "ACE!@#";  // string literal
 
 	cout << s1 << '\n';
 	cout << ps1 << '\n';
-	
+
+	cout << strlen(s1) << '\n';
+	cout << strlen(ps1) << '\n';
+
+	cout << myStrlen(&s1[0]) << '\n';
+	cout << myStrlen(&ps1[0]) << '\n';
+
+	cout << myStrlen(s1) << '\n';
+	cout << myStrlen(ps1) << '\n';
+
 	return 0;
 }
