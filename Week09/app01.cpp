0958fdbfaf1dc92b3d25d1edd5beed85237d9d2b
Dragon-0124 <skseog0841@gmail.com>
2026-04-30 11:41:22 +0900

    Week09: String V0.4 - String Literal

--- Week09/app01.cpp
+++ Week09/app01.cpp
@@ -3,22 +3,11 @@
 using namespace std;
 
 int main() {
-	//char s1[] = { 'A', 'C', 'E', '\0' };
-	//char s1[] = { 'A', 'C', 'E'};  // !!!
-	//char s1[] = { 'A', 'C', 'E', NULL };	
-	//char s1[] = { 'A', 'C', 'E', 0 };
-	char s1[] = "ACE";
-	
-	char* ps1 = &s1[0];			// ≒ char* ps1 = s1;
-	cout << s1 << '\n';			// ACE
-	cout << *ps1 << '\n';		// A
-	cout << ps1 << '\n';		// ACE
-	cout << *s1 << '\n';		// A
-	cout << s1[1] << '\n';		// C
-	cout << *(ps1+1) << '\n';	// C
+	char s1[] = "ACE";			// array compact initializer
+	const char* ps1 = "ACE";	// string literal
 
+	cout << s1 << '\n';
+	cout << ps1 << '\n';
 	
-	ps1[2] = 'Z';				// ≒ *(ps1 + 2) = 'Z';
-	cout << s1 << '\n';			// ACZ
 	return 0;
 }
