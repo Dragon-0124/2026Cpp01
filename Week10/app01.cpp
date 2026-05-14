155f75a15758c74f6f53123e904496745404f7f3
Dragon-0124 <skseog0841@gmail.com>
2026-05-14 10:31:41 +0900

    Week10: String v1.0

--- Week10/app01.cpp
+++ Week10/app01.cpp
@@ -5,21 +5,18 @@ using namespace std;
 
 int main()
 {
-    //char* p = "ACE";   // string literal, error!!
+    int i;
+    cin >> i;
+    int* a = new int[i];
 
-    const char* p = "ACE";  // string literal(constant), read only
-    cout << p << '\n';
+    a[0] = 'A';
+    a[1] = NULL;
 
-    char a[] = "Hi";  // compact initializer, char a[] = {'H','i','\0'};
-    const char b[] = "Hi";  // compact initializer
-    const char* c = "Hi";  // string literal
-
-    a[1] = 'e';
     cout << a << '\n';
-    //b[1] = 'e';
-    //cout << b << '\n';
-    cout << c << '\n';
-    cout << c[1] << '\n';
+    cout << a[0] << '\n';
+
+    delete[] a;
+    a = nullptr;
 
     return 0;
 }
